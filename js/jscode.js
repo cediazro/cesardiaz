000961e7d6e70d004f76eb657008b205a5e6285a
cediazro <rasec.kun@hotmail.com>
2018-07-05 04:29:31 -0500

    vercion 3.0

---
+++ js/jscode.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+$(document).ready(paginacargada);
+
+function paginacargada()
+{
+	
+	$("#barra1").click (accion1);
+	$("#").click (accion2);
+	$("#").click (accion3);
+	
+}
+
+function accion1()
+{
+	
+	$("#nav").slideToggle(1000);
+	
+}
+
+function accion2()
+{
+	
+	$("#texto2").slideToggle(1000);
+	
+}
+function accion3()
+{
+	
+	$("#texto3").slideToggle(1000);
+	
+}
